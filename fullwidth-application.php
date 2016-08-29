f37e1b12cb488d2537799f587d3d600cd832d660
vivlong <vivlonglz@gmail.com>
2016-08-29 17:47:34 +0800

    Update fullwidth-application.php

---
+++ fullwidth-application.php
@@ -0,0 +1,61 @@
+<?php //Template Name:Full-Width-Application Page
+get_header();
+//get_template_part('breadcrums'); ?>
+<div class="container">
+	<div class="row kadima_blog_wrapper">
+		<div class="col-md-12" style="float: none;margin-left:auto;margin-right:auto;">
+			<?php get_template_part('post','page'); ?>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12" style="float: none;margin-left:auto;margin-right:auto;">
+			<div class="kadima_heading_title">
+				<h1>Commercial Lighting</h1>
+				<hr style="margin-top: -1em;margin-bottom: 0px; border: 0;width: 60%;border-top: 1px solid #0e88e3;"/>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row" >
+			<div class="kadima_photo_gallery">
+				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 pull-left">
+					<div class="img-wrapper">
+						<div>
+							<img class="kadima_img_responsive" style="border: 1px solid #D8D8D8;" src="../wp-content/uploads/2016/08/vi01.jpg">
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 pull-left">
+					<div class="img-wrapper">
+						<div>
+							<img class="kadima_img_responsive" style="border: 1px solid #D8D8D8;" src="../wp-content/uploads/2016/08/vi02.jpg">
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 pull-left">
+					<div class="img-wrapper">
+						<div>
+							<img class="kadima_img_responsive" style="border: 1px solid #D8D8D8;" src="../wp-content/uploads/2016/08/vi03.jpg">
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 pull-left">
+					<div class="img-wrapper">
+						<div>
+							<img class="kadima_img_responsive" style="border: 1px solid #D8D8D8;" src="../wp-content/uploads/2016/08/vi05.jpg">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12" style="float: none;margin-left:auto;margin-right:auto;">
+			<div class="kadima_heading_title">
+				<h1>Industrial Lighting</h1>
+				<hr style="margin-top: -1em;margin-bottom: 0px; border: 0;width: 60%;border-top: 1px solid #0e88e3;"/>
+			</div>
+		</div>
+	</div>
+</div>
+<?php get_footer(); ?>
