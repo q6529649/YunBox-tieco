6c75a070925b9fcdbb8826f828af2d59c45f3316
vivlong <vivlonglz@gmail.com>
2016-09-08 11:19:37 +0800

    Update woocommerce/breadcrums.php

---
+++ breadcrums.php
@@ -0,0 +1,10 @@
+<!--div class="kadima_header_breadcrum_title">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1><?php //if(is_home()){echo "";}else{the_title();} ?></h1>
+				<?php//if (function_exists('kadima_breadcrumbs')) kadima_breadcrumbs(); ?>
+			</div>
+		</div>
+	</div>
+</div-->
