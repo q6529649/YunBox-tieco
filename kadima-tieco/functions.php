2fa78d516216ec6b83b5ca6c4d1ef1a28f75f57f
vivlong <vivlonglz@gmail.com>
2016-10-24 19:07:34 +0800

    Init

---
+++ kadima-tieco/functions.php
@@ -0,0 +1,13 @@
+<?php
+function kadima_enqueue_styles() {
+    /* If using a child theme, auto-load the parent theme style. */
+    if ( is_child_theme() ) {
+        wp_enqueue_style( 'parent-style', trailingslashit( get_template_directory_uri() ) . 'style.css' );
+    }
+    /* Always load active theme's style.css. */
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+    //wp_dequeue_style( 'parent-style' );
+}
+add_action( 'wp_enqueue_scripts', 'kadima_enqueue_styles' );
+add_filter( 'loop_shop_per_page', create_function( '$cols', 'return 12;' ), 20 );
+?>
